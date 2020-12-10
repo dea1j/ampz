8f369aa1413fccdd45dc9b9afa4945d41f3b37af
shina <rasheedshinaopeyemi@gmail.com>
2020-12-10 18:16:18 +0100

    addded

---
+++ .cache/sync-requires.js
@@ -0,0 +1,16 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("C:\\Users\\User\\Desktop\\AMPZ FRONTEND\\ampz\\.cache\\dev-404-page.js"))),
+  "component---src-pages-404-js": hot(preferDefault(require("C:\\Users\\User\\Desktop\\AMPZ FRONTEND\\ampz\\src\\pages\\404.js"))),
+  "component---src-pages-about-us-js": hot(preferDefault(require("C:\\Users\\User\\Desktop\\AMPZ FRONTEND\\ampz\\src\\pages\\about-us.js"))),
+  "component---src-pages-get-started-js": hot(preferDefault(require("C:\\Users\\User\\Desktop\\AMPZ FRONTEND\\ampz\\src\\pages\\get-started.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("C:\\Users\\User\\Desktop\\AMPZ FRONTEND\\ampz\\src\\pages\\index.js"))),
+  "component---src-pages-products-js": hot(preferDefault(require("C:\\Users\\User\\Desktop\\AMPZ FRONTEND\\ampz\\src\\pages\\products.js"))),
+  "component---src-pages-using-typescript-tsx": hot(preferDefault(require("C:\\Users\\User\\Desktop\\AMPZ FRONTEND\\ampz\\src\\pages\\using-typescript.tsx")))
+}
+
